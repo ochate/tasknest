de8706973f43e864be187254ab1e210d51e7d516
ochate <gmnftmdssagk@gmail.com>
2025-06-12 16:02:33 +0900

    新規タスク作成画面へのリンク、タスク登録テンプレート作成

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),
+    path('create/', views.task_create, name='task_create'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Task
+from .forms import TaskForm
 
 @login_required
 def task_list(request):
     tasks = Task.objects.filter(user=request.user)
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+@login_required
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_form.html', {'form': form})
