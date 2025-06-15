3cfc3bb669f4a8c26073b7a8c117ccf13e3c553a
ochate <gmnftmdssagk@gmail.com>
2025-06-15 23:10:15 +0900

    accountsアプリの表示を変更

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,7 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('task_list')
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form':form})
