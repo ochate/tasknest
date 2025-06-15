bc55cc876f27cdaa331107fb8de667a4bd0d0b4e
ochate <gmnftmdssagk@gmail.com>
2025-06-15 13:07:33 +0900

    タスク登録削除後のメッセージ表示を追加

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages
 
 # タスク一覧
 @login_required
@@ -18,6 +19,7 @@ def task_create(request):
             task = form.save(commit=False)
             task.user = request.user
             task.save()
+            messages.success(request, 'タスクを作成しました。')
             return redirect('task_list')
     else:
         form = TaskForm()
@@ -37,6 +39,7 @@ def task_edit(request, pk):
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
+            messages.success(request, 'タスクを編集しました。')
             return redirect('task_detail', pk=task.pk)
     else:
         form = TaskForm(instance=task)
@@ -48,5 +51,6 @@ def task_delete(request, pk):
     task = get_object_or_404(Task, pk=pk, user=request.user)
     if request.method == "POST":
         task.delete()
+        messages.success(request, 'タスクを削除しました。')
         return redirect('task_list')
     return render(request, 'tasks/task_confirm_delete.html', {'task': task})
